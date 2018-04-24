6dc8b252c2608cd7a8d662b234dc6e65aba45188
Joel Margolis <joel@teknogeek.io>
2018-04-24 11:42:10 -0400

    Add comments, increase speed, remove IPv6 resolvers that might break, and fix bugs

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # fresh.py
 
 A remix of [fresh.sh](https://github.com/almroot/fresh.sh) with threading and some added benefits :)
+
+# Installation
+Tested on Python 3.5+
+
+```
+$ git clone https://github.com/teknogeek/fresh.py
+$ cd fresh.py/
+$ pip install dnspython
+$ ./fresh.py
+```
--- fresh.py
+++ fresh.py
@@ -5,55 +5,53 @@ from dns.exception import DNSException
 import requests
 import re
 from multiprocessing.pool import ThreadPool
-from threading import Lock
 
 
 NS_LIST_URL = 'http://public-dns.info/nameservers.txt'
 QUERY_DOMAIN = 'example.com'
-FALSE_POSITIVES = [r'^198\.105\.\d{1,3}\.11$']
+FALSE_POSITIVES = [
+    r'^(198\.105\.\d{1,3}\.11)$',
+    r'^((?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$'
+]
 FALSE_POSITIVE_DOMAINS = ['vpncloud.example.com']
-SANITY_CHECK = None
-LOCK = Lock()
-
-FAIL_COUNT = 0
 
 
 def perform_lookup(server, domain, timeout=1, tries=1):
-    global FAIL_COUNT
-    
+    # create DNS resolver
     dns_resolver = Resolver()
     dns_resolver.nameservers = [server]
     dns_resolver.lifetime = timeout
 
+    # try [tries] times
     for _ in range(0, tries):
         try:
             answer = dns_resolver.query(domain)
             return [r.to_text() for r in answer]
-        except DNSException as e:
+        except (DNSException, ValueError):
             pass
     
-    FAIL_COUNT += 1
     return None
 
 
+# called when the check_resolver function thread returns
 def callback(resolver):
     if resolver is not None:
-        print(f'Discovered {resolver}')
+        print('Discovered {}'.format(resolver))
         with open('resolvers.txt', 'a') as f:
-            f.write(f'{resolver}\n')
+            f.write('{}\n'.format(resolver))
 
-def check_nameserver(resolver):
-    global SANITY_CHECK, QUERY_DOMAIN, FALSE_POSITIVE_DOMAINS
 
+# check a DNS resolver with example.com
+def check_resolver(resolver, sanity_check):
     result = perform_lookup(resolver, QUERY_DOMAIN)
-    if result and set(result) == SANITY_CHECK:
+    if result and set(result) == sanity_check:
         # verify that the resolver does not return invalid data
         fp_match = False
         for fp_domain in FALSE_POSITIVE_DOMAINS:
             fp_result = perform_lookup(resolver, fp_domain)
             if fp_result is not None:
                 # if an invalid lookup response is returned, indicate it
-                print(f'Aborting {resolver}: Resolved invalid against {fp_domain}')
+                print('Aborting {}: Resolved invalid against {}'.format(resolver, fp_domain))
                 fp_match = True
                 break
 
@@ -61,38 +59,38 @@ def check_nameserver(resolver):
         if not fp_match:
             return resolver
 
-
     return None
 
-def main():
-    global SANITY_CHECK
 
+def main():
+    # get the list of resolvers
     res = requests.get(NS_LIST_URL)
     if res.status_code == 200:
-        sanity_check_result = perform_lookup('1.1.1.1', QUERY_DOMAIN, tries=5)
+        # perform a baseline test to compare against
+        sanity_check = perform_lookup('1.1.1.1', QUERY_DOMAIN, tries=5)
 
-        if sanity_check_result is not None:
-            SANITY_CHECK = set(sanity_check_result)
+        if sanity_check is not None:
+            sanity_check = set(sanity_check)
+
             # remove false positives
             initial_resolvers = []
             for line in res.content.decode().splitlines():
-                for regex in FALSE_POSITIVES:
-                    if re.sub(regex, '', line):
-                        initial_resolvers.append(line)
-            
-            pool = ThreadPool(20)
+                replace_result = [bool(re.sub(regex, '', line)) for regex in FALSE_POSITIVES]
+                if all(replace_result):
+                    initial_resolvers.append(line)
+
+            # create a thread pool and start the workers
+            thread_pool = ThreadPool(50)
             workers = []
             for resolver in initial_resolvers:
-                workers.append(pool.apply_async(check_nameserver, (resolver,), callback=callback))
-            pool.close()
-            pool.join()
+                workers.append(thread_pool.apply_async(check_resolver, (resolver, sanity_check), callback=callback))
             
-            #filtered_resolvers = list(filter(None, valid_resolvers))
+            # ensure all workers complete
+            for w in workers:
+                w.get()
 
-
-            #print(filtered_resolvers)
-            #with open('resolvers.txt', 'w') as f:
-            #    f.write('\n'.join(filtered_resolvers))
+            thread_pool.close()
+            thread_pool.join()
         else:
             print('Error performing sanity check! (DNS lookup example.com from 1.1.1.1')
 
